d2291bd8bb1aeb8491ac514b593fc77ddfe193a5
lmoore <lmooreintech2021@gmail.com>
2021-05-27 16:42:35 -0400

    hwpush

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -18,16 +19,12 @@ const userSchema = new mongoose.Schema({
 
 
 //fire a function after doc saved to database
-userSchema.pre('save', function (next) {
-    console.log('user about to be created & 
- ]\ saved', this);
-    next();
-});
- 
-userSchema.post('save', function (doc, next){
-    console.log('new user was created and saved', doc);
-    next();
-});
+userSchema.pre('save', async function (next) {
+const salt = await bcrypt.genSalt();
+this.password = await bcrypt.hash(this.password, salt);
+next();
+});    
+
 
 //fire a function before doc saved to database
 
