e39cb9bfde749faf220e1e2acf86ab23db41bb8e
lmoore <lmooreintech2021@gmail.com>
2021-06-01 19:23:57 -0400

    14

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,21 @@ const handleErrors = (err) => {
   let errors = {email: '', password: ''};
 
 
+
+//incorrect email
+if (err.message === 'incorrect email') {
+  error.email = 'that email is not registered';
+}
+
+
+
+//incorrect password
+if (err.message === 'incorrect password') {
+  errors.password = 'that password is incorrect';
+}
+
+
+
   //duplicate error code
 if(err.code === 11000){
   errors.email = 'that email is already registered!';
@@ -56,6 +71,15 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
   const {email, password} = req.body;
 
-  console.log(email,password)
-  res.send('user login');
+try{
+const user = await User.login(email, password);
+const token = createToken(user._id); res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+res.status(200).json({ user: user._id });
+
+}
+catch(err) {
+  const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 }
